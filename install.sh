7805b768abf9d2620a7140e96ea846f0d7e88ea1
Sherwin Sim <sherwin.sim@temasys.com.sg>
2015-05-12 15:32:50 -0400

    Adding ZSH

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "You should install zsh...."
+echo "sudo curl -L http://install.ohmyz.sh | sh"
+
 echo "Installing symlinks..."
 ln -sf ${PWD}/vimrc ${HOME}/.vimrc
 ln -sf ${PWD}/screenrc ${HOME}/.screenrc
+ln -sf ${PWD}/zshrc ${HOME}/.zshrc
